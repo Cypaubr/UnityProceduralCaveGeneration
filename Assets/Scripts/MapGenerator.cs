1eccce86720d685442154787451ba5db06599b85
Cyprien Aubry <cyprienaubry@linkpulsion.com>
2018-07-09 17:02:52 +0200

    Mesh generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-    public int width, height;
+    public int width;
+    public int height;
 
     public string seed;
     public bool useRandomSeed;
 
-    [Range(0,100)]
+    [Range(0, 100)]
     public int randomFillPercent;
 
     int[,] map;
-    
+
     void Start()
     {
         GenerateMap();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
     }
 
     void GenerateMap()
@@ -29,7 +38,11 @@ public class MapGenerator : MonoBehaviour {
         {
             SmoothMap();
         }
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(map, 1);
     }
+
 
     void RandomFillMap()
     {
@@ -44,10 +57,11 @@ public class MapGenerator : MonoBehaviour {
         {
             for (int y = 0; y < height; y++)
             {
-                if (x == 0 || x == width-1 || y == 0 || y == height - 1)
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
                 {
                     map[x, y] = 1;
-                } else
+                }
+                else
                 {
                     map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
                 }
@@ -75,7 +89,7 @@ public class MapGenerator : MonoBehaviour {
     int GetSurroundingWallCount(int gridX, int gridY)
     {
         int wallCount = 0;
-        for (int neighbourX = gridX -1; neighbourX <= gridX + 1; neighbourX++)
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
         {
             for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
             {
@@ -92,22 +106,23 @@ public class MapGenerator : MonoBehaviour {
                 }
             }
         }
+
         return wallCount;
     }
 
+
     void OnDrawGizmos()
     {
-        if (map != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
+        /*
+        if (map != null) {
+            for (int x = 0; x < width; x ++) {
+                for (int y = 0; y < height; y ++) {
+                    Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+                    Vector3 pos = new Vector3(-width/2 + x + .5f,0, -height/2 + y+.5f);
+                    Gizmos.DrawCube(pos,Vector3.one);
                 }
             }
         }
+        */
     }
 }
